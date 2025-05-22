806882ca1b83200f4a6ec05646bdd66c047941e0
Ada Mantic <ada@cantor.connect.lan>
2025-05-23 00:16:13 +0100

    don't try to install tmux - it's already installed.

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs packages from fedora repos
-dnf5 install -y tmux htop virt-viewer iotop-c psacct 
+dnf5 install -y htop virt-viewer iotop-c psacct 
 
 # Use a COPR Example:
 #
